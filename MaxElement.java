9c217e7e9deebd4b41a2903b1f0191c50854993f
saikumar <113576928+vamsi5059@users.noreply.github.com>
2024-03-15 21:44:25 +0530

    Create MaxElement.java

---
+++ MaxElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MaxElement {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        int maxElement = findMaxElement(array);
+        System.out.println("Maximum element in the array: " + maxElement);
+    }
+    public static int findMaxElement(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+}
